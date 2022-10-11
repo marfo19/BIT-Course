e58638417b272a598c8323b35ada3bce332d8e64
marfo19 <104007118+marfo19@users.noreply.github.com>
2022-10-11 23:23:31 +0200

    Update Test 01.js

--- 11.10. Exercises/Test 01.js
+++ 11.10. Exercises/Test 01.js
@@ -1,3 +1,6 @@
+/*1. Check if a triangle is equilateral, scalene or isosceles, and print which triangle is yours.. Example: sides
+of triangle are 3, 4, 5. This is scalene triangle.*/
+
 let x = 1;
 let y = 3;
 let z = 3;
