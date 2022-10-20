b1668c5cd35202ae2ff3154e7d214a6d0c544a67
DESKTOP-I7Q5440\Marko <markofoljan90@gmail.com>
2022-10-20 17:58:16 +0200

    test 07 finished

--- 18.10. Exercises-Loops/loops-test07.js
+++ 18.10. Exercises-Loops/loops-test07.js
@@ -1,1 +1,10 @@
 // 7. Write a code to find on which index in array is letter "C" and show that index on screen. Given array is: [1, 3, 32, $, g, h, t, s, c, 66, 23, h2].
+
+var myArray = [1, 3, 32, "$", "g", "h", "t", "s", "c", 66, 23, "h2"];
+var index = "";
+
+for (i = 0; i <= myArray.length; i++) {
+    if (myArray[i] == "c") {
+        console.log("Index of letter C is " + i);
+    }
+}
